08d1059d0cfe94f2f6fb29ca8439a7d384dd8c50
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-15 17:47:10 -0800

    created getTenRolls and get RandomLetter methods

--- PuzzleJava.java
+++ PuzzleJava.java
@@ -6,13 +6,17 @@ public class PuzzleJava {
     
     public ArrayList<Integer> getTenRolls(int bound){
         ArrayList<Integer> rollResults = new ArrayList<Integer>();
-        for(int id = 0; id < 9; id++){
+        for(int id = 0; id < 10; id++){
             Integer rollResult = randMachine.nextInt(bound);
             rollResults.add(rollResult);
         }
         return rollResults;
     }
     
-    public ArrayList
+    public char getRandomLetter(){
+        String randLetterString = "abcdefghijklmnopqrstuvwxyz";
+        char randLetterResult = randLetterString.charAt(randMachine.nextInt(25));
+        return randLetterResult;
+    }
 
     }
--- TestPuzzleJava.java
+++ TestPuzzleJava.java
@@ -5,8 +5,14 @@ public class TestPuzzleJava {
     public static void main(String[] args){
         
         PuzzleJava generator = new PuzzleJava();
+
+        // ---- getTenRolls Test ----
 		ArrayList<Integer> randomRolls = generator.getTenRolls(20);
 		System.out.println(randomRolls);
+
+        // ---- getRandomLetter Test ----
+        System.out.println("Your random letter is " + generator.getRandomLetter());
+
     }
 }
 
